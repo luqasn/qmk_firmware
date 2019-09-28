6d08a2b2fae7fee62d10625784396701b7858ec5
Lucas <luqasn@gmail.com>
2019-09-28 14:01:06 +0200

    Add my keymap

---
+++ keyboards/planck/keymaps/luqasn/rules.mk
@@ -0,0 +1,5 @@
+MOUSEKEY_ENABLE = no      # Mouse keys(+4700)
+CONSOLE_ENABLE = no         # Console for debug(+400)
+COMMAND_ENABLE = no       # Commands for debug and configuration
+NKRO_ENABLE = no            # Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
+AUDIO_ENABLE = no           # Audio output on port C6
